77e6825cd57c6af7a190b584ed915d28844d28fd
iffarurrahat <iffarurrahat@gmail.com>
2023-12-31 16:23:44 +0600

    firebase config file upadte

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD0LEC61COi1r-nrFsnLpo9A_3C9LWv0IE",
-  authDomain: "recat-dragonnews-auth-revision.firebaseapp.com",
-  projectId: "recat-dragonnews-auth-revision",
-  storageBucket: "recat-dragonnews-auth-revision.appspot.com",
-  messagingSenderId: "369987314542",
-  appId: "1:369987314542:web:8fed01f5e2dec8bd84977f"
+  apiKey: import.meta.env. VITE_APIKEY,
+  authDomain: import.meta.env. VITE_AUTHDOMAIN,
+  projectId: import.meta.env. VITE_PROJECTID,
+  storageBucket: import.meta.env. VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env. VITE_MESSAGINGSENDERID,
+  appId: import.meta.env. VITE_APPID,
 };
 
 // Initialize Firebase
